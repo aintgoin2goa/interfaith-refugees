20d4114a4d2b67fce3157aaed803f504d883edd5
Paul Wilson <paul.i.wilson@ft.com>
2016-09-05 21:19:07 +0100

    fix data

--- scripts/transform.js
+++ scripts/transform.js
@@ -5,17 +5,25 @@ const _ = require('lodash');
 const regexs = new Map();
 
 regexs.set('bishop', /bishop/i);
-regexs.set('honors', /(CBE|MBE|OBE|Lord|Lady)/);
+regexs.set('lords', /(Lord|Lady)/);
+regexs.set('honors', /(CBE|MBE|OBE)/);
+regexs.set('rowanWilliams', /Dr Rowan Williams/i);
 
 
 function transform(d){
 	let data = _.cloneDeep(d);
+	let first = [];
+	let lords = [];
 	let bishops = [];
 	let honors = [];
 	let noPost = [];
 	let theRest = [];
-	data.forEach((item, index) => {
-		if(regexs.get('bishop').test(item.post)){
+	data.forEach((item) => {
+		if(regexs.get('rowanWilliams').test(item.name)) {
+			first.push(item);
+		}else if(regexs.get('lords').test(item.name)){
+			lords.push(item);
+		}else if(regexs.get('bishop').test(item.post)){
 			bishops.push(item);
 		}else if(regexs.get('honors').test(item.name)){
 			honors.push(item);
@@ -26,7 +34,7 @@ function transform(d){
 		}
 	});
 
-	return bishops.concat(honors).concat(theRest).concat(noPost);
+	return first.concat(lords).concat(bishops).concat(honors).concat(theRest).concat(noPost);
 }
 
 module.exports = transform;
